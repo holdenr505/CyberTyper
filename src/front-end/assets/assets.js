aec11626602606389cec69d0ce10ef707e7041f7
holdenr505 <holdenr505@gmail.com>
2025-03-13 21:13:52 -0400

    Add assets.js

---
+++ src/front-end/assets/assets.js
@@ -0,0 +1,11 @@
+import purpleCar from './purple_car.svg';
+import redCar from './red_car.svg';
+
+const assets = {
+    cars: {
+        purple: purpleCar,
+        red: redCar,
+    },
+};
+
+export default assets;
